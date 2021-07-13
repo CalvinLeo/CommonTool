29b652367bd1d1fc90725f31286d572aa8aac9b5
zhaoxiang <gaozhaoxiang@qdama.cn>
2021-07-13 14:28:33 +0800

    example

--- CommonTool/ViewController.swift
+++ CommonTool/ViewController.swift
@@ -11,9 +11,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let auth = ZXAuthView()
+        self.view.addSubview(auth)
+        auth.snp.makeConstraints { make in
+            make.left.equalTo(32)
+            make.right.equalTo(-32)
+            make.height.equalTo(authCodeViewHeight)
+            make.top.equalTo(100)
+        }
+        
+        auth.layoutIfNeeded()
+        auth.codeNumbers = 4
+        auth.becomeFirstRegister()
     }
-
-
 }
 
